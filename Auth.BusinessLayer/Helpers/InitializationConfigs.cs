5c2c8fa196de587845a3926897574ce051f00a33
Stepa <cntgf.2010@yandex.ru>
2022-04-11 17:08:52 +0300

    fixup! Add sending a message on unsuccessful initialization of configurations

--- Auth.BusinessLayer/Helpers/InitializationConfigs.cs
+++ Auth.BusinessLayer/Helpers/InitializationConfigs.cs
@@ -27,7 +27,6 @@ public class InitializationConfigs : IInitializationConfigs
 
     public void InitializeConfigs()
     {
-        _logger.LogInformation($"Attempt to initialize configs from {Microservice.MarvelousConfigs} service");
         _logger.LogInformation("Initialize from default config");
         _config["BaseAddress"] = "80.78.240.16";
         _config[$"{Microservice.MarvelousCrm}Url"] = "https://piter-education.ru:5050";
@@ -37,6 +36,7 @@ public class InitializationConfigs : IInitializationConfigs
 
         try
         {
+            _logger.LogInformation($"Attempt to initialize configs from {Microservice.MarvelousConfigs} service");
             var response = _requestHelper.SendRequest<IEnumerable<ConfigResponseModel>>(_config[$"{Microservice.MarvelousConfigs}Url"],
                 ConfigsEndpoints.Configs,
                 Microservice.MarvelousConfigs,
